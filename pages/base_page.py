232f6d7fc818d1bdce45ecd4fcfe37e7bb7eaa8c
victory-sokolov <viktorsokolov.and@gmail.com>
2020-09-27 23:49:30 +0300

    modify wait_element and imports

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,11 @@
-from config import Config
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
-from utils.Driver import Driver
+from selenium.webdriver.common.by import By
 
+from utils.Driver import Driver
+from config import Config
 
 class BasePage:
 
@@ -20,10 +21,10 @@ class BasePage:
     def find_element(self, *locator):
         return self.driver.find_element(*locator)
 
-    def wait_element(self, delay=10, *locator):
-        from selenium.webdriver.common.by import By
+    def wait_element(self, *locator, delay=10):
+        
         try:
-            WebDriverWait(self.driver, delay).until(
+            return WebDriverWait(self.driver, delay).until(
                 EC.presence_of_element_located((locator)))
         except TimeoutException:
             print("\n * ELEMENT NOT FOUND WITHIN GIVEN TIME! --> %s" %
