26c70da8f3ca2130dbc3f6c73c2b9213e9c58d64
victory-sokolov <viktorsokolov.and@gmail.com>
2020-10-13 22:06:42 +0300

    add logger and run_process method

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}.py
@@ -1,30 +1,51 @@
 import os
+import logging
+from datetime import datetime
 from random import randint
 from time import sleep
 
-from selenium.common.exceptions import TimeoutException, WebDriverException
-from selenium.webdriver.support.wait import WebDriverWait
-
 from pages.base_page import BasePage
-from pages.locators import MainPageLocators
 from pages.login_page import LoginPage
 
 from utils.Driver import Driver
-from utils.helpers import get_file_entries
 from config import Config
+from pages.login_page import LoginPage
+from pages.main_page import MainPage
+
+if not os.path.exists('debug'):
+    os.mkdir('debug')
+
+logging.basicConfig(filename='debug/Teezily.log',
+                    level=logging.ERROR,
+                    format='%(asctime)s %(levelname)s %(name)s %(message)s'
+                    )
+logger = logging.getLogger(__name__)
 
 class Base(BasePage):
 
     def __init__(self, driver: Driver):
-        self.locators = MainPageLocators
+        self.driver = driver
         super().__init__(driver=driver)
 
-    def auth(self):
-        self.driver.get(f'{self.url}/auth')
-        LoginPage(self.driver).sign_in()
+    def run_process(self):
+        try:
+            self.driver.get(self.url)
+            sleep(5)
+            if 'sign_in' in self.driver.current_url:
+                LoginPage(self.driver).sign_in()
+                sleep(30)
+
+            main = MainPage(self.driver)
+            main.email_popup()
+            main.upload_design()
+        except Exception as err:
+            now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            self.driver.save_screenshot(f'debug/{now}.png')
+            logger.error(err)
+            print(err)
 
 
-driver = Driver.chrome() if Config.BROWSER == 'Chrome' else Driver.firefox()
-
-base = Base(driver)
-base.auth()
+if __name__ == "__main__":
+    driver = Driver.chrome()
+    base = Base(driver)
+    base.run_process()
